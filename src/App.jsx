7d88b2b5bf830772c70883f96c0fdca35b949a3a
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-01-08 00:28:44 -0800

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/plan' element={<Plan/>}/>
+            <Route path='/plan' element={<Plan/>}/>
         </Routes>
         <Footer/>
     </div>
