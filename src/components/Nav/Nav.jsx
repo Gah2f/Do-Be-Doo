ef7a6a6471d283f136a967ae672a20a33664925f
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-01-07 23:59:24 -0800

    Nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css'
+function Nav() {
+  return (
+    <div className='nav'>
+       <a href=""> <h1>Link 1</h1> </a> 
+       <a href=""> <h1>Link 2</h1> </a> 
+       <a href=""> <h1>Link 3</h1> </a> 
+    </div>
+  )
+}
+
+export default Nav
